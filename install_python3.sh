9c4ba2f72bf70a09ed7bf10844b0178fcff29e48
huohu <huohu@helijia.com>
2019-05-21 13:29:15 +0800

    site_packages

--- install_python3.sh
+++ install_python3.sh
@@ -12,27 +12,29 @@ python3 --version
 cur_dir=`pwd`
 echo $cur_dir
 
-echo -e "boost lib\n"
+echo -e "\nboost lib\n"
 mkdir -p /usr/boost/
 cp -r $cur_dir/boost/lib /usr/boost/
 
-echo -e "/usr/boost/lib\n" > /etc/ld.so.conf.d/boost.conf
+echo -e "\n/usr/boost/lib\n" > /etc/ld.so.conf.d/boost.conf
 ldconfig
 ldconfig -v | grep boost
 ldconfig -v | grep python
 
-echo -e "libonsclient4cpp.so\n"
+echo -e "\nlibonsclient4cpp.so\n"
 mkdir -p /usr/aliyun/lib
 cp $cur_dir/lib/libonsclient4cpp.so /usr/aliyun/lib
 
-echo -e "/usr/aliyun/lib\n" > /etc/ld.so.conf.d/aliyun.conf
+echo -e "\n/usr/aliyun/lib\n" > /etc/ld.so.conf.d/aliyun.conf
 ldconfig
 ldconfig -v | grep libonsclient4cpp
 
-echo -e "libaliyunmqclientpython.so\n"
-site_packages=`python3 -c "import site; print(site.getsitepackages()[0])"`
+echo -e "\nlibaliyunmqclientpython.so\n"
+# site_packages=`python3 -c "import site; print(site.getsitepackages()[0])"`
+site_packages=`python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
+echo $site_packages
 cp $cur_dir/lib/python3/libaliyunmqclientpython.so $site_packages
 python3 -c "import libaliyunmqclientpython; print('succeed to import libaliyunmqclientpython')"
 
-echo -e "log dir ...\n"
+echo -e "\nlog dir ...\n"
 mkdir -p $HOME/logs/metaq-client4cpp
