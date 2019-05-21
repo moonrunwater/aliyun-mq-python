6e009d17364d05cee8b795d0a578969d4770073b
huohu <huohu@helijia.com>
2019-05-21 14:04:45 +0800

    del set_env.py

--- sample/set_env.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-print("__________Python Version:___________")
-print(sys.version)
-sys.path.append('/usr/local/alibaba/mq/lib')
-print("______Add Path /usr/local/alibaba/mq/lib_______")
